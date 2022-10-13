5b621d3411b63708081faf7447f8d263b0163e44
rabradley <71164560+rabradley@users.noreply.github.com>
2022-10-12 21:30:59 -0500

    Fixed warning.

--- ASDraw.py
+++ ASDraw.py
@@ -8,39 +8,41 @@ def main():
 	X = []
 	Y = []
 	edges = []
-	G = nx.Graph()
-	ASSet = PrefixToAS()
+	graph = nx.Graph()
+	set_AS = PrefixToAS()
 
 	in_file = "ASlinks.txt"
 	with open(in_file) as in_file:
 		line = in_file.readline()
 		while line != "":
-			# Make sure the line is a valid set of data
+			# Make sure the line is uncommented
 			if line.startswith("#"):
 				line = in_file.readline()
 				continue
 			line_chunks = line.split("|")
-			if line_chunks[0] in ASSet or line_chunks[1] in ASSet: 
+			if line_chunks[0] in set_AS or line_chunks[1] in set_AS: 
 				edge = (line_chunks[0], line_chunks[1])
 				if edge not in edges:
 					X.append(line_chunks[0])
 					Y.append(line_chunks[1])
 					edges.append(edge)
-					G.add_edge(line_chunks[0], line_chunks[1], color=colors[random.randint(0, 4)], weight=1)
+					graph.add_edge(line_chunks[0], line_chunks[1], color=colors[random.randint(0, 4)], weight=1)
 			line = in_file.readline()
 
 	plt.rcParams["figure.figsize"] = [7.50, 3.50]
-	plt.rcParams["figure.autolayout"] = True
-
-	colors = nx.get_edge_attributes(G, 'color').values()
-	weights = nx.get_edge_attributes(G, 'weight').values()
-	pos = nx.circular_layout(G)
-	nx.draw(G, pos,
-			edge_color=colors,
-			width=list(weights),
-			with_labels=True,
-			node_color='lightgreen')
-	# nx.draw(G, with_labels=True, node_size=150, alpha=0.5, linewidths=40)
+	#plt.rcParams["figure.autolayout"] = True # Leads to the "This figure includes Axes that are not compatible with tight_layout, so results might be incorrect." 
+	
+	colors = nx.get_edge_attributes(graph, 'color').values()
+	weights = nx.get_edge_attributes(graph, 'weight').values()
+	pos = nx.circular_layout(graph)
+	nx.draw(
+		graph, 
+		pos,
+		edge_color=colors,
+		width=list(weights),
+		with_labels=True,
+		node_color='lightgreen'
+	)
 	plt.show()
 
 
