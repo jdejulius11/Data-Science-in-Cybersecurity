06cfed62d718c071569ec0d101c91915f30f21aa
iTzPsychozTi <krichardson3@mail.bradley.edu>
2022-10-27 16:09:01 -0500

    Updated/broken code

--- softwareApp.py
+++ softwareApp.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QAction
 from PyQt5.QtGui import QIcon
 
 
@@ -11,6 +12,7 @@ class App(QWidget):
 		self.top = 10
 		self.width = 640
 		self.height = 480
+		self.createMenu()
 		self.initUI()
 
 	def initUI(self):
@@ -18,8 +20,30 @@ class App(QWidget):
 		self.setGeometry(self.left, self.top, self.width, self.height)
 		self.show()
 
+	def createMenu(self):
+		headerMenu = self.menuBar()
+
+		# header menus
+		fileMenu = headerMenu.addMenu('File')
+		editMenu = headerMenu.addMenu('Edit')
+		viewMenu = headerMenu.addMenu('View')
+		helpMenu = headerMenu.addMenu('Help')
+
+		# Actions within 'Edit' menu
+		copyAction = QAction(QIcon("Copy.png"), 'Copy', self)
+		copyAction.setShortcut("Ctrl + C")
+		pasteAction = QAction(QIcon("Paste.png"), 'Paste', self)
+		pasteAction.setShortcut("Ctrl + V")
+		editMenu.addAction(copyAction)
+		editMenu.addAction(pasteAction)
+
+		# Actions within 'File' menu
+		saveAction = QAction(QIcon("Save.png"), 'Save', self)
+		saveAction.setShortcut("Ctrl + S")
+		fileMenu.addAction(saveAction)
+
 
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
-	ex = App()
-	sys.exit(app.exec_())
+	App = QApplication(sys.argv)
+	app = App()
+	sys.exit(App.exec_())
